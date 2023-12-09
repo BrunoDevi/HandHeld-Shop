b18a725a0a75c8a32576310b8cd3d2f7e9e9cdbb
Bruno Baêta <brunoantonio25@outlook.com>
2023-12-09 05:22:26 -0300

    add menu-arrows

--- index.js
+++ index.js
@@ -5,10 +5,26 @@ function Top() { // adiciona a funcão de voltar ao topo da pagina
 var botaoTopo = document.querySelector('.botao-topo');
 
 window.addEventListener('scroll', function() {
-    // Verifica se a posição de rolagem é maior que a metade da altura da janela
+    // Verifica se a posição de rolagem é maior que a 1/4 da altura da janela
     if (window.scrollY > window.innerHeight / 4) {
-        botaoTopo.style.opacity = 1; // Torna o botão visível
+        botaoTopo.style.opacity = 1;
     } else {
-        botaoTopo.style.opacity = 0; // Torna o botão invisível
+        botaoTopo.style.opacity = 0;
+    }
+});
+
+// Obtém referências para o input e a imagem
+var inputCheckbox = document.getElementById('topic__product');
+var arrowImage = document.getElementById('arrowImage');
+
+// Adiciona um ouvinte de eventos para detectar alterações no estado do input
+inputCheckbox.addEventListener('change', function() {
+    // Verifica se o input está marcado
+    if (inputCheckbox.checked) {
+        // Troca o src da imagem para a seta para cima
+        arrowImage.src = './img/cabeçalho/icons8-seta-para-cima-30.png';
+    } else {
+        // Troca o src da imagem de volta para a seta para baixo
+        arrowImage.src = './img/cabeçalho/icons8-seta-para-baixo-30.png';
     }
 });
